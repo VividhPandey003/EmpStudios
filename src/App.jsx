ae3a45d5cab8c6918fc8585897d975c9ec86591c
Vividh Pandey <vividh.pandey.13@gmail.com>
2024-04-19 00:47:52 +0530

    ui practice✨
    Signed-off-by: Vividh Pandey <vividh.pandey.13@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Feature from './components/Feature.jsx'
 import './index.css'
+import Workflow from './components/Workflow.jsx'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Hero />
+        <Feature />
+        <Workflow />
+      </div>
+    </>
   )
 }
 
