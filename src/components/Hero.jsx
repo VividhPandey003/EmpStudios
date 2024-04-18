4ce62e6f25edb26b728b6fe81be860b8c713fef4
Vividh Pandey <vividh.pandey.13@gmail.com>
2024-04-19 02:15:18 +0530

    final ui✨👾
    Signed-off-by: Vividh Pandey <vividh.pandey.13@gmail.com>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <div className="flex flex-col items-center mt-6 lg:mt-20">
             <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-                VirtualR build tools
+                EmpStudios builds tools
                 <span className='bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text'>{" "}for developers</span>
             </h1>
             <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>
